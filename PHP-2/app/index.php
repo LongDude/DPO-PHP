c2ea13b1af106a6eda510abce57dff1966063c74
LongDude <teplov.vladimir.2004@mail.ru>
2025-03-01 00:36:18 +0300

    ADD: Database connection. Ready for main task

--- PHP-2/app/index.php
+++ PHP-2/app/index.php
@@ -1,8 +1,14 @@
 <?php
-    if (isset($_ENV['TEST_MESSAGE'])){
-        echo $_ENV['TEST_MESSAGE'];
-    }
-    else {
-        echo 'NO_ENV_SET';
+    $host = 'db';
+    $dbname = $_ENV['DB_NAME'];
+    $dbuser = $_ENV['DB_USER'];
+    $dbpass = $_ENV['DB_PASSWORD'];
+    $port = '5432';
+    print("$dbuser $dbpass\n");
+    $conn = pg_connect("host=$host port=$port dbname=$dbname user=$dbuser password=$dbpass");
+    if (!$conn) {
+        echo "Connection failed";
+    } else {
+        echo "Success";
     }
 ?>
